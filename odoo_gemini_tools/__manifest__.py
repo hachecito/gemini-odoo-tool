f5b226b34883dc75ebe533ef1dc7a7b54a892e6e
hachecito <yhasmanivm@gmail.com>
2024-08-14 19:40:57 -0400

    odoo gemini build

---
+++ odoo_gemini_tools/__manifest__.py
@@ -0,0 +1,25 @@
+
+{
+    'name': "Odoo Gemini Tools",
+    'summary': "Odoo Gemini IA Integration",
+    'author': "Yhasmani Valdes Migenes, OCTUPUS TECHNOLOGIES S.L",
+    'category': 'Tools',
+    'version': '17.0.0.0.1',
+    'depends': ['base', 'web'],
+    'license': 'LGPL-3',
+    'external_dependencies': {
+        'python': ['google-generativeai'],
+    },
+    'data': [
+        'views/views.xml',
+    ],
+    'assets': {
+        'web_editor.backend_assets_wysiwyg': [
+            'odoo_gemini_tools/static/src/js/wysiwyg/**/*',
+        ],
+        'web_editor.assets_wysiwyg': [
+            'odoo_gemini_tools/static/src/lib/showdown.min.js',
+        ],
+    },
+}
+
---
+++ odoo_gemini_tools/models/__init__.py
@@ -0,0 +1,3 @@
+
+# from . import gemini_connector
+from . import res_config_settings
